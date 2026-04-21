ecd299b0431c594e89982bddf633da7541b4d49e
Raphael Alvim <raphaelalvim@gmail.com>
2026-04-21 11:49:40 +0100

    infra: add Databricks setup notebooks and SQL scripts for catalog, schemas and permissions

---
+++ 02_infra/02_databricks/00_setup/01_create_catalog.sql
@@ -0,0 +1,2 @@
+-- Databricks notebook source
+CREATE CATALOG IF NOT EXISTS ptfrozenfoods_dev;
---
+++ 02_infra/02_databricks/00_setup/02_create_schemas.py
@@ -0,0 +1,5 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC CREATE SCHEMA IF NOT EXISTS ptfrozenfoods_dev.bronze;
+# MAGIC CREATE SCHEMA IF NOT EXISTS ptfrozenfoods_dev.silver;
+# MAGIC CREATE SCHEMA IF NOT EXISTS ptfrozenfoods_dev.gold;
